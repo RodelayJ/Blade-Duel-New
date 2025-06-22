1a6d298cff451b1db31337cbdc5628c835aee8e1
RodelayJ <rodelayjerome@gmail.com>
2025-06-22 00:01:05 -0400

    Delete inventory.js

--- inventory.js
+++
@@ -1,6 +0,0 @@
-let inventory = ["Healing Potion", "Mana Elixir"];
-function updateInventory() {
-  console.log('Inventory updated');
-  const inv = document.getElementById("inventory-ui");
-  inv.innerText = "Inventory: " + (inventory.length ? inventory.join(", ") : "Empty");
-}
